3c11ec16aa80ff8ede6b36e104da585e2ac9834e
Fauzan My <bangpozan@gmail.com>
2023-11-15 14:36:03 +0700

    replace _s_

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -16,20 +16,20 @@
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php
-			_s_posted_on();
-			_s_posted_by();
+			pehtheme_wp_posted_on();
+			pehtheme_wp_posted_by();
 			?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php _s_post_thumbnail(); ?>
+	<?php pehtheme_wp_post_thumbnail(); ?>
 
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
 	<footer class="entry-footer">
-		<?php _s_entry_footer(); ?>
+		<?php pehtheme_wp_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
