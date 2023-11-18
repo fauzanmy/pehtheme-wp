38946bfca3099219a95354ea43dae27780b4042a
fauzan My <bangpozan@gmail.com>
2023-11-18 21:15:23 +0700

    Completed the meta author, date, and share button sections on the WordPress theme
    I have just finished implementing the meta author, date, and share button sections on each post in the WordPress theme. The author's information and post date are now displayed correctly, and a share button has been added to facilitate content sharing. All changes have been tested and are ready for integration. #theme #wordpress #metainfo #sharebutton

--- template-parts/content.php
+++ template-parts/content.php
@@ -14,9 +14,9 @@
 	<header class="entry-header mb-8">
 		<?php
 		if ( is_singular() ) :
-			the_title( '<h1 class="text-4xl font-bold leading-normal">', '</h1>' );
+			the_title( '<h1 class="text-2xl lg:text-4xl font-bold leading-normal">', '</h1>' );
 		else :
-			the_title( '<h2 class="text-3xl font-bold leading-normal"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h2 class="text-2xl lg:text-3xl font-bold leading-normal"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
 		if ( 'post' === get_post_type() ) :
@@ -27,10 +27,17 @@
 					<?php echo get_avatar( get_the_author_meta( 'ID' ), 72 ); ?>
 				</div>
 
-				<ul class="insert-h-scroll">
-					<li><?php echo get_the_author_meta('display_name'); ?></li>
-					<li>• August 15, 2023 • 3 min read </li>
+				<ul>
+					<li><a class="font-semibold capitalize" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" title="<?php echo esc_attr( get_the_author() ); ?>"><?php the_author(); ?></a></li>
+					<li><time class="text-sm" datetime="<?php echo get_post_time('c', true); ?>" title="Written and Updated on <?php echo get_post_time('c', true); ?>"><?php the_time('j F Y'); ?></time></li>
 				</ul>
+				<div class="ml-auto shrink-0">
+					<button class="flex items-center border rounded-full p-2 bg-zinc-100 hover:bg-zinc-200 toggle-button leading-none">
+						<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16">
+<path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"></path>
+</svg>
+					</button>
+				</div>
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
