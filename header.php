de73d47d00b8ce390f6761b7bac11e44c497bde6
fauzan My <bangpozan@gmail.com>
2023-11-26 19:35:06 +0700

    Completed the Primary menus horizontal style. I have just finished implementing the primary menus. The main menus are now displayed correctly. All changes have been tested and are ready for integration. #theme #wordpress #horizontal-menu

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 
 					<div class="flex items-center"><!-- left burger-logo -->
 
-						<button class="flex items-center space-x-2 rounded-full border py-1 pr-[5px] pl-3 group bg-zinc-100 hover:bg-zinc-200 toggle-button" data-target="menu-bar">
+						<button class="flex items-center space-x-2 rounded-full border py-1 pr-[5px] pl-3 group bg-zinc-100 hover:bg-zinc-200 toggle-button" data-target="site-navigation">
 							<svg width="22" height="22" fill="currentColor" viewBox="0 0 16 16">
 								<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"></path>
 							</svg>
@@ -65,13 +65,13 @@
 					</div><!-- right search-icon -->
 				</div><!-- /site-branding -->
 
-				<nav id="site-navigation" class="block mt-3 close">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'pehtheme-wp' ); ?></button>
+				<nav id="site-navigation" class="primary-menu block mt-3 close">
+					<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'pehtheme-wp' ); ?></button> -->
 					<?php
 					wp_nav_menu(
 						array(
 							'theme_location' => 'menu-1',
-							'menu_id'        => 'primary-menu',
+							'menu_class'        => 'insert-h-scroll space-x-4',
 						)
 					);
 					?>
